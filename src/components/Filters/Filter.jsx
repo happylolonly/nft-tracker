a793746b64b3a8106e8c98aa6dbf82dca834c86f
Pavel Sumbaev <p-sumbaev@yandex.ru>
2021-09-25 13:57:00 +0300

    fix issues

--- src/components/Filters/Filter.jsx
+++ src/components/Filters/Filter.jsx
@@ -1,16 +1,27 @@
-import React from "react";
-import { AnimatePresence, motion } from "framer-motion";
-import classes from "./Filter.module.scss";
-import FilterIcon from "../Icons/FilterIcon/FilterIcon";
+import React from 'react';
+import { AnimatePresence, motion } from 'framer-motion';
+import classes from './Filter.module.scss';
+import FilterIcon from '../Icons/FilterIcon/FilterIcon';
 
-const Filter = ({showFilters,filterOpenHandler}) => {
+const Filter = ({ showFilters, filterOpenHandler }) => {
   return (
     <AnimatePresence>
-    {showFilters && <motion.div initial={{y: "-100%"}} animate={{y: "0%"}} exit={{y: "-100%"}} className={classes.filter}>
-      <FilterIcon onClick={()=> {filterOpenHandler(false)}}/>
-    </motion.div>}
+      {showFilters && (
+        <motion.div
+          initial={{ y: '-100%' }}
+          animate={{ y: '0%' }}
+          exit={{ y: '-100%' }}
+          className={classes.filter}
+        >
+          <FilterIcon
+            onClick={() => {
+              filterOpenHandler(false);
+            }}
+          />
+        </motion.div>
+      )}
     </AnimatePresence>
-  )
-}
+  );
+};
 
-export default Filter
+export default Filter;
