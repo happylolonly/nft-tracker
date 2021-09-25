b94d3c439be18c9ec6f5a11ae9917b37ccdf93bc
Pavel Sumbaev <p-sumbaev@yandex.ru>
2021-09-25 13:57:40 +0300

    fix issues

--- src/components/Header/HeaderComponent.js
+++ src/components/Header/HeaderComponent.js
@@ -1,37 +1,41 @@
-import React, { useState } from "react";
-import { useMoralis } from "react-moralis";
-import classes from "./HeaderComponent.module.scss";
-import BurgerBtn from "../Icons/BurgerBtn/BurgerBtn";
-import CircleComponent from "../Icons/CircleComponent/CircleComponent";
+import React, { useState } from 'react';
+import { useMoralis } from 'react-moralis';
+import classes from './HeaderComponent.module.scss';
+import BurgerBtn from '../Icons/BurgerBtn/BurgerBtn';
+import CircleComponent from '../Icons/CircleComponent/CircleComponent';
 import { useLocation } from 'react-router-dom';
 import RefreshIcon from '../Icons/RefreshIcon/RefreshIcon';
 import FilterIcon from '../Icons/FilterIcon/FilterIcon';
-import Filter from "../Filters/Filter";
+import Filter from '../Filters/Filter';
 
-const HeaderComponent = ({prev}) => {
+const HeaderComponent = ({ prev }) => {
   const location = useLocation();
-  const [{showFilters}, setState ] = useState({showFilters: false})
-  const currentRoute = location.pathname.split('/').slice(-1)[0]
+  const [{ showFilters }, setState] = useState({ showFilters: false });
+  const currentRoute = location.pathname.split('/').slice(-1)[0];
   const { logout } = useMoralis();
-  const filterOpenHandler = (value= !showFilters)=>{
-    setState(prevState=>({...prevState,showFilters:value}))
-  }
+  const filterOpenHandler = (value = !showFilters) => {
+    setState((prevState) => ({ ...prevState, showFilters: value }));
+  };
 
   return (
     <header className={classes.header}>
       {currentRoute === 'home' ? (
-          <div className={classes.headerInner}>
-            <RefreshIcon onClick={prev} />
-            <span className={classes.mainTitle}>Tracker</span>
-            <FilterIcon onClick={()=> {filterOpenHandler()}}/>
-            <Filter filterOpenHandler={filterOpenHandler} showFilters={showFilters} />
-          </div>
-      ) :
+        <div className={classes.headerInner}>
+          <RefreshIcon onClick={prev} />
+          <span className={classes.mainTitle}>Tracker</span>
+          <FilterIcon
+            onClick={() => {
+              filterOpenHandler();
+            }}
+          />
+          <Filter filterOpenHandler={filterOpenHandler} showFilters={showFilters} />
+        </div>
+      ) : (
         <>
           <CircleComponent />
           <BurgerBtn onLogout={logout} />
         </>
-      }
+      )}
     </header>
   );
 };
